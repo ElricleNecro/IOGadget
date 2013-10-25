b321510a84ba9db2523380be5acd4c1c72b13d1c
ElricleNecro <guiplum@gmail.com>
2013-10-25 10:02:01 +0200

    Add C++ support (__cplusplus macro) + namespace. Add Specialized files which will contain some work around function (like sorting function, swap function, ...).

--- include/IOGadget/gadget.h
+++ include/IOGadget/gadget.h
@@ -1,8 +1,19 @@
 #ifndef _IOGADGET_H
 #define _IOGADGET_H
 
+#ifdef __cplusplus
+namespace iogadget {
+	extern "C" {
+#endif
+
 #include "IOGadget/types.h"
+#include "IOGadget/other.h"
 #include "IOGadget/gadget_read.h"
 #include "IOGadget/gadget_write.h"
+
+#ifdef __cplusplus
+	}
+}
+#endif
 
 #endif /* end of include guard */
