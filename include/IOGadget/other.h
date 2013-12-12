f72d51c94e910a06cf57ef3388de9ba2b125c580
ElricleNecro <guiplum@gmail.com>
2013-12-12 19:06:29 +0100

    Add type differenciation to Swap function.

--- include/IOGadget/other.h
+++ include/IOGadget/other.h
@@ -5,6 +5,7 @@
 
 #include "IOGadget/types.h"
 
-void Swap(Particule a, Particule b);
+void Float_Swap(Particule_f a, Particule_f b);
+void Double_Swap(Particule_d a, Particule_d b);
 
 #endif /* end of include guard */
--- src/other.c
+++ src/other.c
@@ -1,8 +1,17 @@
 #include "IOGadget/other.h"
 
-void Swap(Particule a, Particule b)
+void Double_Swap(Particule_d a, Particule_d b)
 {
-	struct _particule_data tmp;
+	struct _particule_data_d tmp;
+
+	memcpy(&tmp, a, sizeof(tmp));
+	memcpy(a, b, sizeof(tmp));
+	memcpy(b, &tmp, sizeof(tmp));
+}
+
+void Float_Swap(Particule_f a, Particule_f b)
+{
+	struct _particule_data_f tmp;
 
 	memcpy(&tmp, a, sizeof(tmp));
 	memcpy(a, b, sizeof(tmp));
